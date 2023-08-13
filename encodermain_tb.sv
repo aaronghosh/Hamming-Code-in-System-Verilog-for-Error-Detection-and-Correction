78da88613c7d5c700d50ffe97e6bd4117eb2e917
Aaron G <124378527+aaronghosh@users.noreply.github.com>
2023-08-13 12:42:33 +0530

    testbench for encodermain.sv

--- encodermain_tb.sv
+++ encodermain_tb.sv
@@ -1,14 +1,12 @@
 module encodermain_tb;
 
-    // Declare signals for testbench
     reg clk = 0;
     reg write = 0;
     reg data_in = 0;
     reg reset = 1;
     wire ready;
     wire [6:0] data_outp;
-
-    // Instantiate the encodermain module
+    
     encodermain uut (
         .clk(clk),
         .write(write),
@@ -18,26 +16,19 @@ module encodermain_tb;
         .data_out(data_outp)
     );
     
-    // Set the clock period
     parameter clk_period = 100;
 
-    // Generate clock signal
     always begin
         # (clk_period / 2);
         clk = ~clk;
     end
 
-    // Initialize signals and perform tests
     initial begin
-        // Initialize inputs
         data_in = 0;
         write = 0;
         reset = 1;
-        
-        // Wait a few clock cycles
         # (clk_period * 5);
         
-        // Release reset
         reset = 0;
         
         // Test 1: Encode '1010'
@@ -46,7 +37,7 @@ module encodermain_tb;
         # (clk_period);
         write = 0;
         # (clk_period);
-        data_in = 0; // Reset data
+        data_in = 0; 
         write = 1;
         # (clk_period);
         write = 0;
@@ -55,9 +46,8 @@ module encodermain_tb;
         write = 1;
         # (clk_period);
         write = 0;
-        # (clk_period * 2); // Wait for encoding
+        # (clk_period * 2); 
         
-        // Display encoded output
         $display("Encoded output for '1010': %b", data_outp);
 
         // Test 2: Encode '1001'
@@ -66,7 +56,7 @@ module encodermain_tb;
         # (clk_period);
         write = 0;
         # (clk_period);
-        data_in = 0; // Reset data
+        data_in = 0;
         write = 1;
         # (clk_period);
         write = 0;
@@ -75,9 +65,8 @@ module encodermain_tb;
         write = 1;
         # (clk_period);
         write = 0;
-        # (clk_period * 2); // Wait for encoding
+        # (clk_period * 2); 
         
-        // Display encoded output
         $display("Encoded output for '1001': %b", data_outp);
 
         // Test 3: Encode '0110'
@@ -86,7 +75,7 @@ module encodermain_tb;
         # (clk_period);
         write = 0;
         # (clk_period);
-        data_in = 0; // Reset data
+        data_in = 0; 
         write = 1;
         # (clk_period);
         write = 0;
@@ -95,14 +84,11 @@ module encodermain_tb;
         write = 1;
         # (clk_period);
         write = 0;
-        # (clk_period * 2); // Wait for encoding
+        # (clk_period * 2); 
         
-        // Display encoded output
         $display("Encoded output for '0110': %b", data_outp);
         
-        // Add more tests as needed
-        
-        // End simulation
+    
         $finish;
     end
     
